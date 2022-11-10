cde75a2ee669c371083b7581ce5bf7e400e6c665
sauravmondal24 <sauravmondal321@gmail.com>
2022-11-10 16:19:24 +0600

    Project Done

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// user: poviUserData
-// password: AzFH3f2CKoXEdnIY
-
 // MongoDb connected
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xzekhbt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
